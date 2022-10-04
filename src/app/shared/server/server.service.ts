32d55cbdd2be74840803033c069455e5adb3c8cf
beto <beto@auraair.io>
2022-10-04 15:35:26 +0300

    WIP server setup

--- src/app/shared/server/server.service.ts
+++ src/app/shared/server/server.service.ts
@@ -1,10 +1,51 @@
 import { Injectable } from '@angular/core'
+import { IForm } from './server.model'
 
 @Injectable({
   providedIn: 'root',
 })
 export class ServerService {
   public totalVisits: number = 0
+  public forms: IForm[] = []
 
-  constructor() {}
+  constructor() {
+    this.fetchVisits()
+    this.fetchForms()
+  }
+
+  public getVisits(): number {
+    return this.totalVisits
+  }
+
+  public saveForm(form: IForm): void {
+    this.forms.push(form)
+    localStorage.setItem('forms', JSON.stringify(this.forms))
+  }
+
+  public getForms(): IForm[] {
+    return this.forms
+  }
+
+  public addVisit(): void {
+    this.totalVisits++
+    localStorage.setItem('totalVisits', this.totalVisits.toString())
+  }
+
+  private fetchVisits(): void {
+    const totalVisits = localStorage.getItem('totalVisits')
+    if (totalVisits) {
+      this.totalVisits = parseInt(totalVisits)
+    } else {
+      this.totalVisits = 0
+    }
+  }
+
+  private fetchForms(): void {
+    const forms = localStorage.getItem('forms')
+    if (forms) {
+      this.forms = JSON.parse(forms)
+    } else {
+      this.forms = []
+    }
+  }
 }
